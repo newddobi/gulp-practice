e59ba64a709a16d3120ef2e0ec5acac99a978ff0
ddobi <qhdqhdekd261@naver.com>
2022-03-28 20:47:57 +0900

    watch file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,16 +2,32 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
-  pug:{
-    src:"src/*.pug",
-    dest:"build"
-  }
-}
+  pug: {
+    watch: "src/**/*.pug",
+    src: "src/*.pug",
+    dest: "build",
+  },
+};
 
-export const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+const pug = () =>
+  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-export const clean = () => del(["build"]);
+const clean = () => del(["build"]);
 
-export const dev = gulp.series([clean,pug]);
+const webserver = () =>
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
