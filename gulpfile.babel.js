3d748a55b0705641591a81dd50710eb8a8ba6581
ddobi <qhdqhdekd261@naver.com>
2022-03-28 11:18:13 +0900

    add gitignore file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,14 @@
 // const gulp = require("gulp");
 import gulp from "gulp";
+import pug from "gulp-pug";
+
+const routes = {
+  pug:{
+    src:"src/*.pug",
+    dest:"build"
+  }
+}
+
+export const pug = () => gulp.src(routes.pug.src).pipe(pug()).pipe(gulp.dest());
 
 export const dev = () => console.log("I will dev");
